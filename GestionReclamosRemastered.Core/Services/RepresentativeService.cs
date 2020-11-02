e76e7b0c55b4ae51df82f6e4b3e171119a3cb190
Federico Galvan <fedegv.4991@gmail.com>
2020-11-01 21:45:13 -0300

    Add Representative Service

---
+++ GestionReclamosRemastered.Core/Services/RepresentativeService.cs
@@ -0,0 +1,20 @@
+﻿using GestionReclamosRemastered.Core.Interfaces;
+using GestionReclamosRemastered.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionReclamosRemastered.Core.Services
+{
+    class RepresentativeService : IRepresentativeService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public RepresentativeService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        
+    }
+}
