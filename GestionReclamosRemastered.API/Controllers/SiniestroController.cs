79335187d1e1a19d6e7cd0b383b0edb3886beef0
Javier Gonzalez Espejo <javiergespejo@gmail.com>
2020-11-02 14:45:27 -0300

    SiniestroController created

---
+++ GestionReclamosRemastered.API/Controllers/SiniestroController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GestionReclamosRemastered.Core.DTOs;
+using GestionReclamosRemastered.Core.Entities;
+using GestionReclamosRemastered.Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestionReclamosRemastered.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SiniestroController : ControllerBase
+    {
+        private readonly ISiniestroRepository _siniestroRepository;
+        public SiniestroController(ISiniestroRepository siniestroRepository)
+        {
+            _siniestroRepository = siniestroRepository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<SiniestroDto>> GetAllSiniestrosAsync()
+        {
+            var siniestros = await _siniestroRepository.GetAllAsync();
+            return SiniestroDto.SiniestroDtoToEntityList(siniestros);
+        }
+    }
+}
