49c2197073126e704c1e2e7d23738f8ffc8ca197
Javier Gonzalez Espejo <javiergespejo@gmail.com>
2020-11-02 11:14:35 -0300

    Siniestro repository added

---
+++ GestionReclamosRemastered.Infrastructure/Repositories/SiniestroRepository.cs
@@ -0,0 +1,24 @@
+﻿using GestionReclamosRemastered.Core.Entities;
+using GestionReclamosRemastered.Core.Interfaces;
+using GestionReclamosRemastered.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionReclamosRemastered.Infrastructure.Repositories
+{
+    public class SiniestroRepository : GenericRepository<Siniestro>, ISiniestroRepository
+    {
+        public SiniestroRepository(GestionReclamosContext context) : base(context)
+        {
+        }
+
+        public Task<Siniestro> GetSiniestro(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
