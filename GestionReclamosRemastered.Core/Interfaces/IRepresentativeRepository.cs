a18f4a8a3ec7540984c18eec3af9cd7cb2430361
Federico Galvan <fedegv.4991@gmail.com>
2020-11-01 21:44:17 -0300

    Add Representative Repository

---
+++ GestionReclamosRemastered.Core/Interfaces/IRepresentativeRepository.cs
@@ -0,0 +1,11 @@
+﻿using GestionReclamosRemastered.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestionReclamosRemastered.Core.Interfaces
+{
+    public interface IRepresentativeRepository : IGenericRepository<Representante>
+    {
+    }
+}
---
+++ GestionReclamosRemastered.Infrastructure/Repositories/RepresentativeRepository.cs
@@ -0,0 +1,18 @@
+﻿using GestionReclamosRemastered.Core.Entities;
+using GestionReclamosRemastered.Core.Interfaces;
+using GestionReclamosRemastered.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestionReclamosRemastered.Infrastructure.Repositories
+{
+    public class RepresentativeRepository : GenericRepository<Representante>, IRepresentativeRepository
+    {
+        public RepresentativeRepository(GestionReclamosContext context) : base(context)
+        {
+
+        }
+        
+    }
+}
