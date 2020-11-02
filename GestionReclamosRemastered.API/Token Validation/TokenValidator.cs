39fa9b190430cc43758cce65d19b38287aa3aaa8
Federico Galvan <fedegv.4991@gmail.com>
2020-11-01 21:45:24 -0300

    Add token validator

---
+++ GestionReclamosRemastered.API/Token Validation/TokenValidator.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionReclamosRemastered.API.Token_Validation
+{
+    public class TokenValidator
+    {
+        private readonly IConfiguration _configuration;
+        public TokenValidator(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public bool ValidateToken(string token)
+        {
+            var mySecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Authentication:SecretKey"]));
+
+			var tokenHandler = new JwtSecurityTokenHandler();
+			try
+			{
+				tokenHandler.ValidateToken(token, new TokenValidationParameters
+				{
+					ValidateIssuerSigningKey = true,
+					ValidateIssuer = true,
+					ValidateAudience = true,
+					ValidIssuer = _configuration["Authentication:Issuer"],
+					ValidAudience = _configuration["Authentication:Audience"],
+					IssuerSigningKey = mySecurityKey
+				}, out SecurityToken validatedToken);
+			}
+			catch
+			{
+				return false;
+			}
+			return true;
+		}
+}}
