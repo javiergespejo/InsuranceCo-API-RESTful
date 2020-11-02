db624cbbe8e94f572e8c103c50b3d45e1f68a244
Federico Galvan <fedegv.4991@gmail.com>
2020-11-01 21:44:56 -0300

    Add Representative Controller

---
+++ GestionReclamosRemastered.API/Controllers/RepresentativeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using GestionReclamosRemastered.API.Token_Validation;
+using GestionReclamosRemastered.Core.Entities;
+using GestionReclamosRemastered.Core.Interfaces;
+using GestionReclamosRemastered.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace GestionReclamosRemastered.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RepresentativeController : Controller
+    {
+        private readonly IRepresentativeService _representativeService;
+        private readonly IConfiguration _configuration;
+
+
+        public RepresentativeController(IRepresentativeService representativeService, IConfiguration configuration)
+        {
+            _representativeService = representativeService;
+            _configuration = configuration;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Representante>>> GetRepresentatives()
+        {
+            TokenValidator tkValidator = new TokenValidator(_configuration);
+            string token = HttpContext.Request.Headers["Authorization"];
+            if (tkValidator.ValidateToken(token))
+            {
+
+                return Ok();
+            }
+            return NotFound();
+        }
+
+
+    }
+}
