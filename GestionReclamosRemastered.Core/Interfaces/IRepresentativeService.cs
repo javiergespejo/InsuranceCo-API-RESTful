d12e1dc184eaa40b7fe2de377515bb0d59603b48
Federico Galvan <fedegv.4991@gmail.com>
2020-11-02 23:02:51 -0300

    Add logic and methods to Representative Service

--- GestionReclamosRemastered.Core/Interfaces/IRepresentativeService.cs
+++ GestionReclamosRemastered.Core/Interfaces/IRepresentativeService.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using GestionReclamosRemastered.Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GestionReclamosRemastered.Core.Interfaces
 {
     public interface IRepresentativeService
     {
-        
+        IEnumerable<Representante> GetAllRepresentatives();
+        Task<Representante> GetRepresentativeById(int id);
+        Task<bool> UpdateRepresentative(Representante representantive);
+        Task<bool> DeleteRepresentative(Representante representantive);
     }
 }
--- GestionReclamosRemastered.Core/Services/RepresentativeService.cs
+++ GestionReclamosRemastered.Core/Services/RepresentativeService.cs
@@ -2,12 +2,12 @@
 using GestionReclamosRemastered.Core.Entities;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace GestionReclamosRemastered.Core.Services
 {
-    class RepresentativeService : IRepresentativeService
+    public class RepresentativeService : IRepresentativeService
     {
         private readonly IUnitOfWork _unitOfWork;
         public RepresentativeService(IUnitOfWork unitOfWork)
@@ -15,6 +15,59 @@ namespace GestionReclamosRemastered.Core.Services
             _unitOfWork = unitOfWork;
         }
 
-        
+        /// <summary>
+        /// Soft deletes the representative from the database, changing the active state to 1
+        /// </summary>
+        /// <param name="representantive"></param>
+        /// <returns></returns>
+        public async Task<bool> DeleteRepresentative(Representante representative)
+        {
+
+            representative.SnActivo = 1;
+            if (representative.GetType().GetProperties()
+                            .All(p => p.GetValue(representative) != null))
+            {
+                _unitOfWork.RepresentativeRepository.Update(representative);
+                await _unitOfWork.SaveChangesAsync();
+                return true;
+            }
+            throw new Exception();
+        }
+
+        /// <summary>
+        /// Gets a list of active (SnActivo = -1) representatives
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Representante> GetAllRepresentatives()
+        {
+            return _unitOfWork.RepresentativeRepository.GetAll().Where(x => x.SnActivo == -1);
+        }
+
+        /// <summary>
+        /// Gets a representative by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<Representante> GetRepresentativeById(int id)
+        {
+            return await _unitOfWork.RepresentativeRepository.GetById(id);
+        }
+
+        /// <summary>
+        /// Updates a representative information
+        /// </summary>
+        /// <param name="representative"></param>
+        /// <returns></returns>
+        public async Task<bool> UpdateRepresentative(Representante representative)
+        {
+            if (representative.GetType().GetProperties()
+                            .All(p => p.GetValue(representative) != null))
+            {
+                _unitOfWork.RepresentativeRepository.Update(representative);
+                await _unitOfWork.SaveChangesAsync();
+                return true;
+            }
+            throw new Exception();
+        }
     }
 }
