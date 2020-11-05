b0eadd46b08ebea3686dec003da5d42e8b31c2fe
Javier Gonzalez Espejo <javiergespejo@gmail.com>
2020-11-05 11:44:31 -0300

    SiniestroService created

---
+++ GestionReclamosRemastered.Core/Interfaces/ISiniestroService.cs
@@ -0,0 +1,15 @@
+﻿using GestionReclamosRemastered.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionReclamosRemastered.Core.Interfaces
+{
+    public interface ISiniestroService
+    {
+        Task<List<SiniestroDto>> GetAllSiniestros();
+
+        Task<SiniestroDto> GetSiniestroById(long id);
+    }
+}
