5c6a4101f9df9dc7729f7a09ba40704e4b32f34e
Javier Gonzalez Espejo <javiergespejo@gmail.com>
2020-11-11 11:55:55 -0300

    queryFilter options changed

--- GestionReclamosRemastered.Core/QueryFilters/SiniestroQueryFilter.cs
+++ GestionReclamosRemastered.Core/QueryFilters/SiniestroQueryFilter.cs
@@ -6,7 +6,7 @@ namespace GestionReclamosRemastered.Core.QueryFilters
 {
     public class SiniestroQueryFilter
     {
-        public long? NroStro { get; set; }
+        public long? NroSiniestroProteccion { get; set; }
         public string? TxtConductor { get; set; }
         public DateTime? FechaSiniestro { get; set; }
     }
